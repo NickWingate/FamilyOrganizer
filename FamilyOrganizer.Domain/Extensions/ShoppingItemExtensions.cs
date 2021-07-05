1508cc7f2f44be373498b1a79904c673fdf51b08
NickWingate <nicholas.wingate03@gmail.com>
2021-07-05 21:36:03 +0100

    Adds total cost extension methods

---
+++ FamilyOrganizer.Domain/Extensions/ShoppingItemExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FamilyOrganizer.Domain.Entities;
+
+namespace FamilyOrganizer.Domain.Extensions
+{
+	public static class ShoppingItemExtensions
+	{
+		public static decimal TotalCost(this ShoppingItem item)
+		{
+			return item.UnitCost * item.Quantity;
+		}
+	}
+}
---
+++ FamilyOrganizer.Domain/Extensions/ShoppingListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using FamilyOrganizer.Domain.Entities;
+
+namespace FamilyOrganizer.Domain.Extensions
+{
+	public static class ShoppingListExtensions 
+	{
+		public static decimal TotalCost(this ShoppingList list)
+		{
+			return list.Items.Sum(item => item.TotalCost());
+		}
+	}
+}
